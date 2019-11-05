dacaa6ce4912dd76e38820ae6b921a97e4a7ee86
David Bryson <bryson.d@etoncollege.org.uk>
2019-11-05 11:10:54 +0000

    A load of garbage

---
+++ StandardAlgorithms/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  StandardAlgorithms
+//
+//  Created by Bryson, David (IRG) on 05/11/2019.
+//  Copyright © 2019 David Bryson. All rights reserved.
+//
+
+import Foundation
+
+print("Goodbye, World!")
+
+let name = "David Bryson"
+
+print(name)
+
+print ("NO!")
+
